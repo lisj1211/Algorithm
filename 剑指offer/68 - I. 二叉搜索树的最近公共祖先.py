97feccdeaee66b364e7a9b18345397bbc9ab9049
shaojie1211 <512975917@qq.com>
2023-03-07 20:32:30 +0800

    二叉搜索树的最近公共祖先

---
+++ 剑指offer/68 - I. 二叉搜索树的最近公共祖先.py
@@ -0,0 +1,27 @@
+# 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+
+# 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+# 输出: 6
+# 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
+
+
+
+
