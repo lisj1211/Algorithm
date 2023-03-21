3463b828e929c26ca4987f8c35c3db92f7fe65a6
shaojie1211 <512975917@qq.com>
2023-03-21 21:06:15 +0800

    合并两个有序链表

---
+++ LeetCodeHot100/21. 合并两个有序链表.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+# # 来源：力扣（LeetCode）
+# # 链接：https://leetcode.cn/problems/valid-parentheses
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:
+            return list1 if not list2 else list2
+        pre = ListNode()
+        cur = pre
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+
+        cur.next = list1 if not list2 else list2
+
+        return pre.next
