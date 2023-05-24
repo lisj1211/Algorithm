66cd6368ec87683c264891f36439766bc168b686
shaojie1211 <512975917@qq.com>
2023-05-24 21:27:38 +0800

    搜索二维矩阵

---
+++ LeetCodeHot100/240. 搜索二维矩阵 II.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# 编写一个高效的算法来搜索mxn矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+#
+# 每行的元素从左到右升序排列。
+# 每列的元素从上到下升序排列。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode.cn/problems/search-a-2d-matrix-ii
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        i, j = m - 1, 0
+        while i >= 0 and j < n:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                i -= 1
+            else:
+                j += 1
+
+        return False
